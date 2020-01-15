b0dad267f032ec2f7f65d3a567a92444ce9c9dc2
dianamelga <di.melgarejo@gmail.com>
2020-01-14 22:35:28 -0300

    crud libros

--- adminbiblioteca/src/components/libros/EditarLibro.js
+++ adminbiblioteca/src/components/libros/EditarLibro.js
@@ -1,12 +1,140 @@
 import React, { Component } from 'react';
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { Link } from "react-router-dom";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
 class EditarLibro extends Component {
-    state = {}
+    //crear los refs
+    tituloInput = React.createRef();
+    editorialInput = React.createRef();
+    ISBNInput = React.createRef();
+    existenciaInput = React.createRef();
+
+
+    editarLibro = e => {
+        e.preventDefault();
+
+        const libroActualizado = {
+            titulo : this.tituloInput.current.value,
+            editorial : this.editorialInput.current.value,
+            ISBN : this.ISBNInput.current.value,
+            existencia : this.existenciaInput.current.value
+        }
+
+        const { libro, firestore, history } = this.props;
+
+        console.log(libroActualizado);
+
+        firestore.update({
+            collection: 'libros',
+            doc: libro.id
+        }, libroActualizado).then(history.push('/'));
+
+
+    }
+
     render() {
+
+        const { libro } = this.props;
+        if(!libro) return <Spinner/>
+
         return (
-            <h1>Editar Libro</h1>
+            <div className="row">
+            <div className="col-12 mb-4">
+              <Link to="/" className="btn btn-secondary">
+                <i className="fas fa-arrow-circle-left"></i> Volver al listado
+              </Link>
+            </div>
+    
+            <div className="col-12">
+              <h2>
+                <i className="fas fa-book"></i> Editar Libro
+              </h2>
+    
+              <div className="row justify-content-center">
+                <div className="col-md-8 mt-5">
+                  <form onSubmit={this.editarLibro}>
+                    <div className="form-group">
+                      <label>Titulo</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="titulo"
+                        placeholder="Titulo o Nombre del Libro"
+                        required
+                        ref={this.tituloInput}
+                        defaultValue={libro.titulo}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label>Editorial</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="editorial"
+                        placeholder="Editorial del Libro"
+                        required
+                        ref={this.editorialInput}
+                        defaultValue={libro.editorial}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label>ISBN</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="ISBN"
+                        placeholder="ISBN del Libro"
+                        required
+                        ref={this.ISBNInput}
+                        defaultValue={libro.ISBN}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label>Existencia</label>
+                      <input
+                        type="number"
+                        min="0"
+                        className="form-control"
+                        name="existencia"
+                        placeholder="Cantidad existente del Libro"
+                        required
+                        ref={this.existenciaInput}
+                        defaultValue={libro.existencia}
+                      />
+                    </div>
+    
+                    <input
+                      type="submit"
+                      className="btn btn-success"
+                      value="Editar Libro"
+                    />
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
         );
     }
 };
 
-export default EditarLibro;
+EditarLibro.propTypes = {
+    firestore: PropTypes.object.isRequired
+}
+
+
+export default compose(
+    firestoreConnect(props => [
+      {
+        collection: "libros",
+        storeAs: "libro",
+        doc: props.match.params.id
+      }
+    ]),
+    connect(({ firestore: { ordered } }, props) => ({
+      libro: ordered.libro && ordered.libro[0]
+    }))
+  )(EditarLibro);
--- adminbiblioteca/src/components/libros/NuevoLibro.js
+++ adminbiblioteca/src/components/libros/NuevoLibro.js
@@ -1,11 +1,126 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { firestoreConnect } from "react-redux-firebase";
+import PropTypes from "prop-types";
 
 class NuevoLibro extends Component {
-    render () {
-        return (
-            <h1>Nuevo Libro</h1>
-        )
-    }
+  state = {
+    titulo: "",
+    editorial: "",
+    ISBN: "",
+    existencia: "",
+    prestados: []
+  };
+
+  agregarLibro = e => {
+    e.preventDefault();
+
+    //tomar copia del state
+    const nuevoLibro = { ...this.state };
+
+    //extraer firestore con sus metodos
+    const { firestore, history } = this.props;
+
+    //anhadirlo a la bd y redireccionar
+    firestore
+      .add(
+        {
+          collection: "libros"
+        },
+        nuevoLibro
+      )
+      .then(() => history.push("/"));
+  };
+
+  leerDato = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col-12 mb-4">
+          <Link to="/" className="btn btn-secondary">
+            <i className="fas fa-arrow-circle-left"></i> Volver al listado
+          </Link>
+        </div>
+
+        <div className="col-12">
+          <h2>
+            <i className="fas fa-book"></i> Nuevo Libro
+          </h2>
+
+          <div className="row justify-content-center">
+            <div className="col-md-8 mt-5">
+              <form onSubmit={this.agregarLibro}>
+                <div className="form-group">
+                  <label>Titulo</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="titulo"
+                    placeholder="Titulo o Nombre del Libro"
+                    required
+                    value={this.state.titulo}
+                    onChange={this.leerDato}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Editorial</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="editorial"
+                    placeholder="Editorial del Libro"
+                    required
+                    value={this.state.editorial}
+                    onChange={this.leerDato}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>ISBN</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="ISBN"
+                    placeholder="ISBN del Libro"
+                    required
+                    value={this.state.ISBN}
+                    onChange={this.leerDato}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Existencia</label>
+                  <input
+                    type="number"
+                    min="0"
+                    className="form-control"
+                    name="existencia"
+                    placeholder="Cantidad existente del Libro"
+                    required
+                    value={this.state.existencia}
+                    onChange={this.leerDato}
+                  />
+                </div>
+
+                <input
+                  type="submit"
+                  className="btn btn-success"
+                  value="Agregar Libro"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default NuevoLibro;
+NuevoLibro.propTypes = {
+    firestore: PropTypes.object.isRequired
+}
+
+export default firestoreConnect()(NuevoLibro);
